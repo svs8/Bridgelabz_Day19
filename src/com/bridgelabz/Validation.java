30e723f68c8b751866d13b5286c91961cc4713e5
svs8 <surakshithvittalshetty@gmail.com>
2022-05-23 01:11:36 +0530

    Added:Password Validation(Atleast One Number

--- src/com/bridgelabz/Validation.java
+++ src/com/bridgelabz/Validation.java
@@ -5,76 +5,76 @@ import java.util.regex.Pattern;
 
 
 public class Validation {
-//     public static void userFirstName() {
-//
-//        String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter First Name:");
-//        String firstName = scanner.next();
-//
-//        Pattern pattern = Pattern.compile(firstNamePattern);
-//        Matcher matcher = pattern.matcher(firstName);
-//        boolean result = matcher.matches();
-//
-//        if(result == true){
-//            userLastName();
-//        }
-//
-//        else
-//            System.out.println("First Name is Invalid");
-//    }
-//
-//    public static void userLastName() {
-//
-//        String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter Last Name:");
-//        String lastName = scanner.next();
-//
-//        Pattern pattern1 = Pattern.compile(lastNamePattern);
-//        Matcher matcher1 = pattern1.matcher(lastName);
-//        boolean result1 = matcher1.matches();
-//
-//        if(result1 == true)
-//            userEmail();
-//        else
-//            System.out.println("Last Name is Invalid");
-//    }
-//    public static void userEmail(){
-//        String emailPattern= "[a-zA-Z0-9.+-]+[0-9a-zA-Z]*@[a-z0-9]+[.][a-z]{2,4}[.a-z]*";
-//        System.out.println("Enter your Email:");
-//        Scanner scanner = new Scanner(System.in);
-//        String email = scanner.next();
-//
-//        Pattern pattern2=Pattern.compile(emailPattern);
-//        Matcher matcher2=pattern2.matcher(email);
-//        boolean result2=matcher2.matches();
-//
-//        if(result2== true){
-//            mobileNumberValidation();
-//        }else{
-//            System.out.println("Email is not valid");
-//        }
-//
-//    }
-//
-//    public static void mobileNumberValidation(){
-//        Scanner scanner = new Scanner(System.in);
-//         String MobileNumberPattern="(91-)?[0-9]{10}";
-//        System.out.println("Enter the phone number");
-//        String phoneNumber=scanner.next();
-//
-//        Pattern pattern3=Pattern.compile(MobileNumberPattern);
-//        Matcher matcher3=pattern3.matcher(phoneNumber);
-//        Boolean result3=matcher3.matches();
-//
-//        if(result3==true){
-//           passwordValidation();
-//        }else{
-//            System.out.println("Mobile number is invalid");
-//        }
-//
-//    }
+     public static void userFirstName() {
+
+        String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name:");
+        String firstName = scanner.next();
+
+        Pattern pattern = Pattern.compile(firstNamePattern);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean result = matcher.matches();
+
+        if(result == true){
+            userLastName();
+        }
+
+        else
+            System.out.println("First Name is Invalid");
+    }
+
+    public static void userLastName() {
+
+        String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.next();
+
+        Pattern pattern1 = Pattern.compile(lastNamePattern);
+        Matcher matcher1 = pattern1.matcher(lastName);
+        boolean result1 = matcher1.matches();
+
+        if(result1 == true)
+            userEmail();
+        else
+            System.out.println("Last Name is Invalid");
+    }
+    public static void userEmail(){
+        String emailPattern= "[a-zA-Z0-9.+-]+[0-9a-zA-Z]*@[a-z0-9]+[.][a-z]{2,4}[.a-z]*";
+        System.out.println("Enter your Email:");
+        Scanner scanner = new Scanner(System.in);
+        String email = scanner.next();
+
+        Pattern pattern2=Pattern.compile(emailPattern);
+        Matcher matcher2=pattern2.matcher(email);
+        boolean result2=matcher2.matches();
+
+        if(result2== true){
+            mobileNumberValidation();
+        }else{
+            System.out.println("Email is not valid");
+        }
+
+    }
+
+    public static void mobileNumberValidation(){
+        Scanner scanner = new Scanner(System.in);
+         String MobileNumberPattern="(91-)?[0-9]{10}";
+        System.out.println("Enter the phone number");
+        String phoneNumber=scanner.next();
+
+        Pattern pattern3=Pattern.compile(MobileNumberPattern);
+        Matcher matcher3=pattern3.matcher(phoneNumber);
+        Boolean result3=matcher3.matches();
+
+        if(result3==true){
+           passwordValidation1();
+        }else{
+            System.out.println("Mobile number is invalid");
+        }
+
+    }
 
     public static void passwordValidation1(){
         Scanner scanner = new Scanner(System.in);
@@ -105,9 +105,27 @@ public class Validation {
         Boolean result5=matcher5.matches();
 
         if(result5==true){
+            passwordValidation3();
+        }else{
+            System.out.println("Password is invalid,please enter atleast one uppercase");
+        }
+
+    }
+
+    public static void passwordValidation3(){
+        Scanner scanner = new Scanner(System.in);
+        String PasswordPattern3="[A-Z][0-9][a-zA-Z0-9~!@#$]{6,}";
+        System.out.println("Enter the Password to check Rule3");
+        String passWord3=scanner.next();
+
+        Pattern pattern6=Pattern.compile(PasswordPattern3);
+        Matcher matcher6=pattern6.matcher(passWord3);
+        Boolean result6=matcher6.matches();
+
+        if(result6==true){
             System.out.println("Password is valid");
         }else{
-            System.out.println("Password is invalid,please enter minimum 8 character");
+            System.out.println("Password is invalid,please enter atleast one numeric");
         }
 
     }
@@ -115,8 +133,8 @@ public class Validation {
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration program");
-//        userFirstName();
-passwordValidation1();
+       userFirstName();
+
     }
 }
 
